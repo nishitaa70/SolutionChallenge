792f67342b7d5f3e9736ea1569e246731446a448
Abhishek Singhal <86764280+Abhimrt@users.noreply.github.com>
2023-03-09 12:29:30 +0530

    threejs

--- src/App.js
+++ src/App.js
@@ -1,50 +1,72 @@
-import React from 'react'
-import { Route, Routes,Link } from "react-router-dom";
-import Main from './Component/landing page/Main';
+// import React from 'react'
+// import { Route, Routes,Link } from "react-router-dom";
+// import Mainlanding from './Component/landing page/Main';
+// import MainThree from './Component/threeJs/Main';
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <div className='app'>
+//       <nav className="navbar navbar-expand-lg navbar-light bg-light">
+//         <Link className="navbar-brand" href="#">Navbar</Link>
+//         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+
+//         <div className="collapse navbar-collapse" id="navbarSupportedContent">
+//           <ul className="navbar-nav mr-auto">
+//             <li className="nav-item active">
+//               <Link className="nav-link" href="#">Home <span className="sr-only">(current)</span></Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" href="#">Link</Link>
+//             </li>
+//             <li className="nav-item dropdown">
+//               <Link className="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+//                 Dropdown
+//               </Link>
+//               <div className="dropdown-menu">
+//                 <Link className="dropdown-item" href="#">Action</Link>
+//                 <Link className="dropdown-item" href="#">Another action</Link>
+//                 <div className="dropdown-divider"></div>
+//                 <Link className="dropdown-item" href="#">Something else here</Link>
+//               </div>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link disabled">Disabled</Link>
+//             </li>
+//           </ul>
+//           <form className="form-inline my-2 my-lg-0">
+//             <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
+//               <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+//           </form>
+//         </div>
+//       </nav>
+//       <Routes>
+//         {/* <Route path="/" element={<Mainlanding />} /> */}
+//         <Route path="/" element={<MainThree/>} />
+//       </Routes>
+//     </div>
+//   )
+// }
+
+// export default App
+
+// import "./styles.css";
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Suspense } from "react";
+import Model from "./Model";
+
+export default function App() {
   return (
-    <div className='app'>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <Link className="navbar-brand" href="#">Navbar</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active">
-              <Link className="nav-link" href="#">Home <span className="sr-only">(current)</span></Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" href="#">Link</Link>
-            </li>
-            <li className="nav-item dropdown">
-              <Link className="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </Link>
-              <div className="dropdown-menu">
-                <Link className="dropdown-item" href="#">Action</Link>
-                <Link className="dropdown-item" href="#">Another action</Link>
-                <div className="dropdown-divider"></div>
-                <Link className="dropdown-item" href="#">Something else here</Link>
-              </div>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link disabled">Disabled</Link>
-            </li>
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-        </div>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Main />} />
-      </Routes>
+    <div className="App">
+      <Canvas>
+        <Suspense fallback={null}>
+          <Model />
+          <OrbitControls />
+          <Environment preset="sunset" background />
+        </Suspense>
+      </Canvas>
     </div>
-  )
+  );
 }
-
-export default App
---
+++ src/Component/threeJs/Main.js
@@ -0,0 +1,79 @@
+import { Environment, MeshDistortMaterial, OrbitControls, Sphere, useGLTF } from '@react-three/drei'
+import { Canvas, useLoader } from '@react-three/fiber'
+import "./three.css"
+import texture from "../../images/tex1.jpeg"
+import { TextureLoader } from 'three'
+import { Suspense } from 'react'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import Model from '../../Model'
+
+const Main = () => {
+
+    // box
+    const colorMap = useLoader(TextureLoader, texture)
+    const Box = () => {
+        return (
+            <mesh rotation={[23, 53, 23]}>
+                <boxBufferGeometry attach="geometry" args={[3, 3, 3]} />
+                {/* === for color === */}
+                {/* <meshLambertMaterial attach="material" color="aqua" /> */}
+
+                {/* === for texture === */}
+                <meshStandardMaterial map={colorMap} />
+
+                {/* === for RGB coloring === */}
+                <meshNormalMaterial attach="material" />
+            </mesh>
+        )
+    }
+
+    // animated shape
+    const Animate = () => {
+        return (
+            <>
+                <Sphere visible args={[1, 100, 200]} scale={2}>
+                    <MeshDistortMaterial
+                        color="purple"
+                        attach="material"
+                        distort={.6}
+                        speed={5}
+                        roughness={1}
+                    />
+
+
+                </Sphere>
+            </>
+        )
+    }
+   
+
+    return (
+        <div className="threeD">
+            <Canvas>
+                <OrbitControls enableZoom={false} />
+                <ambientLight intensity=".5" />
+                <directionalLight position={[1, 1, 1]} intensity="1" />
+                <Suspense fallback={null}>
+                    <Box />
+                </Suspense>
+            </Canvas >
+            <Canvas>
+                <OrbitControls enableZoom={false} />
+                <ambientLight intensity=".5" />
+                <directionalLight position={[1, 1, 1]} intensity="1" />
+                {/* <Suspense fallback={null}> */}
+                <Animate />
+                {/* </Suspense> */}
+            </Canvas>
+            <Canvas>
+                <Suspense fallback={null}>
+                    <Model />
+                    <OrbitControls />
+                    <Environment preset="sunset" background />
+                </Suspense>
+            </Canvas>
+        </div>
+    );
+}
+
+export default Main
