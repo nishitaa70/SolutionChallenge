48615762240f46c9df0620b1111702dcd0e3d547
Abhishek Singhal <86764280+Abhimrt@users.noreply.github.com>
2023-03-31 15:28:45 +0530

    spelling

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const App = () => {
           }}
         >
           <span style={{fontSize:"3rem"}}>D</span>
-          HARRA
+          HARAA
           </Link>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
